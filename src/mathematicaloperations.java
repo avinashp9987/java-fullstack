ad2d22394dc4ab993348ef9a51e265f8f0058127
avinash <avinashp7588@gmail.com>
2022-06-08 16:07:04 +0530

    mathematicaloperations.java

---
+++ src/mathematicaloperations.java
@@ -0,0 +1,26 @@
+public class mathematicaloperations {
+    public static void main(String[] args) {
+        int x = 52;
+        int y = 85;
+        int result;
+
+        //Addition
+        result = x+y;
+        System.out.println("Addition is"+result);
+
+        //substraction
+
+        result = y-x;
+        System.out.println("substraction is"+result);
+
+        //multiplication
+        result = x*y;
+        System.out.println("multiplication is"+result);
+
+        //dividation
+        result = y/x;
+        System.out.println("dividation is"+result);
+
+
+    }
+}
