68f42d33930c3b15b591c41ee4f8ddce0006e970
avinash <avinashp7588@gmail.com>
2022-06-16 13:59:32 +0530

    Initial Commit

---
+++ src/ControlFlowsStatement/VowelsEx.java
@@ -0,0 +1,39 @@
+package ControlFlowsStatement;
+
+import java.util.Scanner;
+
+public class VowelsEx {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter any character");
+//        char ch = (char) sc.next().charAt(0);  //method
+//        //  String ch1 = sc.next();  IT is also used
+//        switch (ch) {
+//            case 'a':
+//                System.out.println("vowell");
+//                break;
+//            case 'e':
+//                System.out.println("vowel");
+//                break;
+//            case 'i':
+//                System.out.println("vowel");
+//                break;
+//            case 'o':
+//                System.out.println("vowel");
+//                break;
+//            case 'u':
+//                System.out.println("vowel");
+//            default:
+//                System.out.println("consonants");
+//        }
+        //Feb: February
+//        for(;;){
+//        System.out.println("infinite Loop");
+  //  }
+    // Enhanced for loop
+    int a[]= { 5,10,15,20,25};
+            for (int no:a){
+        System.out.println(no);
+    }
+}
+}
